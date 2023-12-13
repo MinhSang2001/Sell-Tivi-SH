c3ac866016c17b75f16c9ba7c9138f22998c98b5
MinhSang2001 <sang24052001@gmail.com>
2023-12-14 00:40:56 +0700

    second

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ import productRoutes from './routes/productRoutes.js'
 import sliderRoutes from './routes/sliderRoutes.js'
 import newRoutes from './routes/newRoutes.js'
 import cors from "cors";
+import { fileURLToPath } from 'url';
 import path from 'path'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // configure env
 dotenv.config({ path: '.env' })
